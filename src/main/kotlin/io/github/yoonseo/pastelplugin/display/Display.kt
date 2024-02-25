0b7f84c51b5ca8fb7d1081643b8cb6be18135d49
yoonseo9057 <juhojuhorty@gmail.com>
2024-02-25 13:57:45 +0900

    FRAME DSL base fin

--- src/main/kotlin/io/github/yoonseo/pastelplugin/display/Display.kt
+++ src/main/kotlin/io/github/yoonseo/pastelplugin/display/Display.kt
@@ -1,33 +1,34 @@
 package io.github.yoonseo.pastelplugin.display
 
-import io.github.yoonseo.pastelplugin.ScheduleRepeating
 import io.github.yoonseo.pastelplugin.debug
-import io.github.yoonseo.pastelplugin.spawn
 import net.kyori.adventure.text.Component
 import net.kyori.adventure.text.TextComponent
 import org.bukkit.Bukkit
 import org.bukkit.Location
-import org.bukkit.entity.Interaction
-import org.bukkit.entity.Player
-import org.bukkit.entity.TextDisplay
-import org.bukkit.util.Vector
-import java.util.EventListener
+import javax.xml.stream.events.Comment
 import kotlin.math.PI
 
 /*
 * first array means Y
 * second means X */
-typealias DisplayingMap = ArrayList<ArrayList<Char>>
+typealias SimpleDisplayingMap = ArrayList<ArrayList<Char>>
+typealias ElementNodeLambda = ElementNode.() -> Unit
 class Display {
+    var size : DisplaySize = DisplaySize(0f,0f)
 
     private val elementList = ArrayList<DisplayElement>()
 
     fun addElement(element: DisplayElement){
         elementList.add(element)
     }
+    fun addAllElement(elements: ArrayList<DisplayElement>){
+        elementList.addAll(elements)
+    }
     fun summon(loc : Location) {
+        val a = loc.clone()
+        a.pitch = 0f
         elementList.forEach {
-            it.summon(it.pos.applyToLocation(loc))
+            it.summon(it.pos.applyToLocation(a))
         }
     }
 
@@ -35,55 +36,127 @@ class Display {
         elementList.forEach { it.close() }
     }
 
-    fun interactiveElement()
+
 
 }
-fun test(){
-    display {
-        define {
-            displayingMap(
-                "        E",
-                "         ",
-                "    E    ",
-                "    E    ",
-                "         ",
-            )
+fun test() : Display{
+    return display {
+        initialize(5f sizedBy 5f)
+
+        displayedBySimpleMap(
+            "E  E  E  E  E  E  E",
+            "E  E  E  E  E  E  E",
+            "E  E  E  E  E  E  E",
+            "E  E  E  E  E  E  E",
+            "E  E  E  E  E  E  E",
+            "E  E  E  E  E  E  E",
+            "E  E  E  E  E  E  E"
+        ){
+
+            repeat(49){
+                textElement("A",Component.text(it),TextElement.HEIGHT)
+            }
+//            interactiveElement("interactive"){
+//                textElement("quit",Component.text("[X]"),TextElement.HEIGHT)
+//            }.whenInteracted {
+//                display.close()
+//            }
+//            interactiveElement("interactive"){
+//                textElement("text",Component.text("경대영"),1f)
+//            }.whenInteracted {
+//                it.sendMessage("바보")
+//            }
+//            interactiveElement("interactive"){
+//                textElement("text",Component.text("바보"),1f)
+//            }.whenInteracted {
+//                it.sendMessage("경대영")
+//            }
         }
-        addElements {
-
-        }
     }
 }
 
-class TopNode{
-    fun define(destinationNode: DestinationNode.() -> DisplayingMap){
+
+interface Node
+class TopNode(val display : Display) : Node{
+    internal val registered = ArrayList<DisplayElement>()
+    fun initialize(size: DisplaySize){
+        display.size = size
+    }
+    fun elements(elementNode: ElementNodeLambda){
 
     }
-    fun addElements(bottomNode: BottomNode.() -> Unit){
-
-    }
-}
-class DestinationNode{
-    fun displayingMap(vararg lines : String) : DisplayingMap{
-        val map = DisplayingMap()
+    fun displayedBySimpleMap(vararg lines : String, elementNode: ElementNodeLambda) : SimpleDisplayingMap{
+        val map = SimpleDisplayingMap()
 
         for((i,e) in lines.withIndex()){
+            map.add(ArrayList())
             for(element in e){
-                if(element != ' ' && element != 'E') throw IllegalArgumentException("only allowed ' ' and 'E'")
+                if(element != 'E' && element != 'N') continue
                 map[i].add(element)
             }
         }
+
+        registered.addAll(quickEject(display,this,elementNode))
+
+
+        val mapHeight = map.size; val mapWidth = map.first().size
+
+        val pixelHeight = display.size.height/mapHeight
+        val pixelWidth = display.size.width/mapWidth
+        debug("$pixelHeight,$pixelWidth")
+
+        var displayElementNumber = 0
+        for(height in map.indices){
+            require(mapHeight == map[height].size)//validation height == width
+
+            for((width,element) in map[height].withIndex()){//위치 적용시키는 코드
+                if(element == 'E'){
+                    try{
+                        registered[displayElementNumber].pos = DisplayPosition(height*pixelHeight,width*pixelWidth)
+                    }catch (e : IndexOutOfBoundsException){
+                        Bukkit.getLogger().warning("number of defined SimpleDisplayedMap's element is not correspond with given number of element")
+                    }
+                    displayElementNumber++
+                }
+            }
+
+        }
+
+
+        display.addAllElement(registered)
         return map
     }
-}
-class BottomNode{
 
+    fun setEventReceiver(){
+
+    }
+}
+
+class ElementNode(val display : Display,val superNode: Node) : Node{
+    internal val registered = ArrayList<DisplayElement>()
+
+    fun interactiveElement( elementName: String,pos: DisplayPosition = DisplayPosition.ZERO,elementNode: ElementNodeLambda) : InteractiveElement{
+        return InteractiveElement(display,pos,elementName).also {registered.add(it); it.inners = quickEject(display,this,elementNode)}
+    }
+
+    fun textElement(elementName: String, text : TextComponent, textWidth : Float, textHeight : Float = TextElement.HEIGHT,pos: DisplayPosition = DisplayPosition.ZERO) : TextElement{
+        return TextElement(display,pos,elementName,text,textWidth, textHeight).also {registered.add(it)}
+    }
+
+
+}
+
+private fun quickEject(display : Display,superNode : Node,elementNode: ElementNodeLambda) : ArrayList<DisplayElement> {
+    val temp = ElementNode(display,superNode)
+    elementNode(temp)
+    return temp.registered
 }
 
 fun display(topNode: TopNode.() -> Unit) : Display{
-    val temp = Display()
-    topNode(temp)
-    return temp
+    val display = Display()
+    val node = TopNode(display)
+    topNode(node)
+    return display
 }
 
 /*
@@ -92,122 +165,10 @@ fun display(topNode: TopNode.() -> Unit) : Display{
 *       TextElement("")
 *
 *   }
-*
-*
-*
-*
-*
-*
-*
-*
-*
-*
-*
-*
-*
-*
-*
-*
-*
-*
-*
-*
-*
-*
-*
 * */
 
-
-
-
-
-
-
-
-
-
-class InteractiveElement(
-    pos: DisplayPosition,
-    elementName: String,
-    val innerElement: DisplayElement?
-) : DisplayElement(pos, elementName) ,Extendable
-{
-    lateinit var whenInteracted : (Player) -> Unit
-
-    lateinit var interaction : Interaction
-    private var lastInteractedTime : Long = -1
-    var eventListenerTask : Int = -1
-    fun whenInteracted(task : (Player) -> Unit){
-        whenInteracted = task
-    }
-    override fun summon(summonLocation: Location) {
-        innerElement?.summon(summonLocation)
-
-
-        interaction = summonLocation.spawn(Interaction::class)
-
-        interaction.apply {
-            setRotation(summonLocation.yaw,0f)
-            isResponsive = true
-
-            if(innerElement != null){
-                interactionWidth = innerElement.getSize().width
-                interactionHeight = innerElement.getSize().height
-
-            }else {
-                interactionWidth = 1f;interactionHeight = 1f
-            }
-            //interactionWidth = 1f;interactionHeight = 1f
-        }
-
-        //interaction 관리
-        eventListenerTask = ScheduleRepeating {
-            if(interaction.lastInteraction != null && interaction.lastInteraction!!.timestamp > lastInteractedTime){
-                whenInteracted(interaction.lastInteraction!!.player.player!!)
-                lastInteractedTime = interaction.lastInteraction!!.timestamp
-            }
-        }
-    }
-
-    override fun close() {
-        innerElement?.close()
-        interaction.remove()
-    }
-
-    override fun getSize(): DisplaySize = innerElement?.getSize() ?: DisplaySize(interaction.interactionWidth,interaction.interactionHeight)
-
-}
-class TextElement(pos: DisplayPosition, elementName: String,val text : TextComponent,val textWidth : Float,val textHeight : Float = HEIGHT) : DisplayElement(pos, elementName) {
-    lateinit var textDisplay : TextDisplay
-    companion object{
-        const val HEIGHT = 0.275f
-    }
-
-    fun showText(component: TextComponent?){
-        textDisplay.text(component)
-    }
-
-    override fun summon(summonLocation: Location) {
-        textDisplay = summonLocation.spawn(TextDisplay::class)
-        textDisplay.apply {
-            text(this@TextElement.text)
-            setRotation(summonLocation.yaw,0f)
-
-            displayWidth = textWidth
-            displayHeight = textHeight
-        }
-    }
-
-    override fun close() {
-        textDisplay.remove()
-    }
-
-    override fun getSize(): DisplaySize = DisplaySize(textDisplay.displayWidth,textDisplay.displayHeight)
-
-}
-
-
 data class DisplayPosition(val x : Float,val y : Float){
+    companion object {val ZERO = DisplayPosition(0f,0f)}
     fun applyToLocation(target : Location) : Location {
         return target.clone()
             .add(0.0, y.toDouble(),0.0)
@@ -217,14 +178,8 @@ data class DisplayPosition(val x : Float,val y : Float){
 
 }
 data class DisplaySize(val width : Float,val height : Float)
-abstract class DisplayElement(val pos : DisplayPosition,val elementName : String){
-    abstract fun summon(summonLocation : Location)
-
-    abstract fun close()
-
-    abstract fun getSize() : DisplaySize
-}
+infix fun Float.sizedBy(f : Float) = DisplaySize(this,f)
 
 interface Extendable {
-    operator fun invoke(extendingNode : (Unit) -> DisplayElement)
+    val inners : ArrayList<DisplayElement>
 }
